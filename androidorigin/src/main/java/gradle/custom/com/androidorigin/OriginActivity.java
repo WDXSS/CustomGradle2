3f38e2073a4eba869f8d0ae6d524bb55497c4027
zhouchao <810753835@qq.com>
2019-01-07 18:45:55 +0800

    提交

---
+++ androidorigin/src/main/java/gradle/custom/com/androidorigin/OriginActivity.java
@@ -0,0 +1,17 @@
+package gradle.custom.com.androidorigin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ * Created by zc on 2019/1/7
+ */
+public class OriginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
