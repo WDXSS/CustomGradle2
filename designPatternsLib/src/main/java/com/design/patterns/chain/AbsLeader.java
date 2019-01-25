8a64a882cd815ad7e83e23f6a1d4d2db87064f47
zhouchao <810753835@qq.com>
2019-01-25 18:38:26 +0800

    责任链模式

---
+++ designPatternsLib/src/main/java/com/design/patterns/chain/AbsLeader.java
@@ -0,0 +1,7 @@
+package com.design.patterns.chain;
+
+/**
+ * Created by zc on 2019/1/25
+ */
+public abstract class AbsLeader {
+}
---
+++ designPatternsLib/src/main/java/com/design/patterns/chain/ChainMain.java
@@ -0,0 +1,18 @@
+package com.design.patterns.chain;
+
+/**
+ * 责任链模式（Chain of Responsibility Pattern）
+ * 定义：
+ * 组成：
+ * Handler：抽象处理对象 abs抽象类
+ *          1.声明请求方法
+ *          2.处理接口
+ *          3.持有下一个节点的引用
+ *          4.设置下一个节点的方法
+ * ConcreteHandler 具体实现
+ * 战：请求涨工资，500元组长可以做主，1000元主管做主，3000元 经理，4000元Boss做主
+ * 1.抽象领导 AbsLeader 对象
+ */
+public class ChainMain {
+
+}
