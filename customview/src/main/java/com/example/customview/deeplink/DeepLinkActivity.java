3d56b4bd1aab0a2e77720ec37b78f6215d711a0b
zhouchao <810753835@qq.com>
2019-08-26 18:12:49 +0800

    权限申请和IMEI和meid

---
+++ customview/src/main/java/com/example/customview/deeplink/DeepLinkActivity.java
@@ -0,0 +1,14 @@
+package com.example.customview.deeplink;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DeepLinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
