1b58fa8d7b9be7b10c0d17fd7b2a8e2dda2a85c2
zhouchao <810753835@qq.com>
2019-10-09 16:09:03 +0800

    版本定制，设置不同的icon和 launcher

--- app/src/main/java/gradle/custom/com/customgradle/MainActivity.java
+++ app/src/main/java/gradle/custom/com/customgradle/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 import gradle.custom.com.androidlibrary.LibActivity;
+
 //那就是 support-annotations 库的 RestrictTo 注解，配合 lint 使用。
 // 比如 MainActivity.java 要禁止外部使用
 @RestrictTo(RestrictTo.Scope.LIBRARY)
