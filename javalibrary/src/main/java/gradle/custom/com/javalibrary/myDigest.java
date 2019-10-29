5c20ca455c0aecf46f3b5cdd93a989f0f93f4a7e
zhouchao <810753835@qq.com>
2019-10-29 17:01:38 +0800

    版本定制，设置不同的icon和 launcher

--- javalibrary/src/main/java/gradle/custom/com/javalibrary/myDigest.java
+++ javalibrary/src/main/java/gradle/custom/com/javalibrary/myDigest.java
@@ -7,45 +7,49 @@ import java.security.MessageDigest;
  * @date 2019/10/17
  */
 public class myDigest {
-    public static void main(String[] args)  {
-        myDigest my=new myDigest();
+
+    public static void main(String[] args) {
+        myDigest my = new myDigest();
         my.testDigest();
     }
+
     public void testDigest() {
         try {
-            String myinfo="我的测试信息";
+            String myinfo = "我的测试信息";
             //java.security.MessageDigest alg=java.security.MessageDigest.getInstance("MD5");
-            MessageDigest alga=MessageDigest.getInstance("SHA-1");
+            MessageDigest alga = MessageDigest.getInstance("SHA-1");
             alga.update(myinfo.getBytes());
-            byte[] digesta=alga.digest();
-            System.out.println("本信息摘要是:"+byte2hex(digesta));
+            byte[] digesta = alga.digest();
+            System.out.println("0本信息摘要是:" + byte2hex(digesta));
+            System.out.println("0本信息摘要是:" + new String(digesta));
             //通过某中方式传给其他人你的信息(myinfo)和摘要(digesta) 对方可以判断是否更改或传输正常
 
 
-            MessageDigest algb=MessageDigest.getInstance("SHA-1");
+            MessageDigest algb = MessageDigest.getInstance("SHA-1");
             algb.update(myinfo.getBytes());
-            if (algb.isEqual(digesta,algb.digest())) {
-                System.out.println("信息检查正常");
+            if (algb.isEqual(digesta, algb.digest())) {
+                System.out.println("1信息检查正常");
             } else {
-                System.out.println("摘要不相同");
+                System.out.println("2摘要不相同");
             }
         } catch (java.security.NoSuchAlgorithmException ex) {
-            System.out.println("非法摘要算法");
+            System.out.println("3非法摘要算法");
         }
     }
+
     public String byte2hex(byte[] b) {
         //二行制转字符
-        String hs="";
-        String stmp="";
-        for (int n=0;n<b.length;n++) {
-            stmp=(Integer.toHexString(b[n] & 0XFF));
-            if (stmp.length()==1) {
-                hs=hs+"0"+stmp;
+        String hs = "";
+        String stmp = "";
+        for (int n = 0; n < b.length; n++) {
+            stmp = (Integer.toHexString(b[n] & 0XFF));
+            if (stmp.length() == 1) {
+                hs = hs + "0" + stmp;
             } else {
-                hs=hs+stmp;
+                hs = hs + stmp;
             }
-            if (n<b.length-1) {
-                hs=hs+":";
+            if (n < b.length - 1) {
+                hs = hs + ":";
             }
         }
         return hs.toUpperCase();
--- ui_system/src/main/java/com/codingending/uisystemdemo/common/EditTextFragment.java
+++ ui_system/src/main/java/com/codingending/uisystemdemo/common/EditTextFragment.java
@@ -2,7 +2,9 @@ package com.codingending.uisystemdemo.common;
 
 import android.app.Fragment;
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
+
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.Spanned;
@@ -25,8 +27,8 @@ import com.codingending.uisystemdemo.R;
  * Created by CodingEnding on 2018/2/11.
  */
 
-public class EditTextFragment extends Fragment{
-    public static final String TAG="EditTextFragment";
+public class EditTextFragment extends Fragment {
+    public static final String TAG = "EditTextFragment";
     private EditText watchWriteEditText;//监听用户输入的EditText
     private EditText styleOmeOptionEditText;//自定义虚拟键盘右下角Enter键
     private EditText codeLimitInputEditText;//通过代码限制输入内容
@@ -34,29 +36,31 @@ public class EditTextFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_edit_text,container,false);
+        View view = inflater.inflate(R.layout.fragment_edit_text, container, false);
         initViews(view);
         return view;
     }
 
-    private void initViews(View view){
-        watchWriteEditText=view.findViewById(R.id.edit_text_watch_write);
-        styleOmeOptionEditText=view.findViewById(R.id.edit_text_style_ime_option);
-        codeLimitInputEditText=view.findViewById(R.id.edit_text_code_limit_input);
+    private void initViews(View view) {
+        watchWriteEditText = view.findViewById(R.id.edit_text_watch_write);
+        styleOmeOptionEditText = view.findViewById(R.id.edit_text_style_ime_option);
+        codeLimitInputEditText = view.findViewById(R.id.edit_text_code_limit_input);
 
         //监听用户的输入
         watchWriteEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                Log.i(TAG,"beforeTextChanged："+s);//内容改变前
+                Log.i(TAG, "beforeTextChanged：" + s);//内容改变前
             }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Log.i(TAG,"onTextChanged："+s);//内容改变时
+                Log.i(TAG, "onTextChanged：" + s);//内容改变时
             }
+
             @Override
             public void afterTextChanged(Editable s) {
-                Log.i(TAG,"afterTextChanged："+s);//内容改变后
+                Log.i(TAG, "afterTextChanged：" + s);//内容改变后
             }
         });
 
@@ -64,8 +68,8 @@ public class EditTextFragment extends Fragment{
         styleOmeOptionEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId== EditorInfo.IME_ACTION_SEARCH){
-                    Toast.makeText(v.getContext(),"点击了搜索按钮",Toast.LENGTH_SHORT).show();
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    Toast.makeText(v.getContext(), "点击了搜索按钮", Toast.LENGTH_SHORT).show();
                     return true;
                 }
                 return false;
@@ -73,11 +77,11 @@ public class EditTextFragment extends Fragment{
         });
 
         //通过代码限制输入内容只能为数字
-        InputFilter filter=new InputFilter() {
+        InputFilter filter = new InputFilter() {
             @Override
             public CharSequence filter(CharSequence source, int start, int end,
                                        Spanned dest, int dstart, int dend) {
-                if(!TextUtils.isDigitsOnly(source)){
+                if (!TextUtils.isDigitsOnly(source)) {
                     return "";//如果当前输入的内容包含数字之外的字符，就默认输入为空字符
                 }
                 return source;
