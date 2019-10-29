a080b47128df1c1c7e273f5b2eb6569bd298eb4f
zhouchao <810753835@qq.com>
2019-10-29 17:03:02 +0800

    test

---
+++ app/src/main/java/gradle/custom/com/customgradle/App.java
@@ -0,0 +1,14 @@
+package gradle.custom.com.customgradle;
+
+import android.app.Application;
+
+/**
+ * @author zhouchao
+ * @date 2019/10/29
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
