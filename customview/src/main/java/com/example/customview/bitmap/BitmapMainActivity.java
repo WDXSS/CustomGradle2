23bdf7b3d7db060ed6c240ffd5150afc5e2599de
zhouchao <810753835@qq.com>
2020-06-09 10:26:00 +0800

    收藏的url

---
+++ customview/src/main/java/com/example/customview/bitmap/BitmapMainActivity.java
@@ -0,0 +1,28 @@
+package com.example.customview.bitmap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.customview.R;
+
+/**
+ * @author zhouchao
+ * @date 2020/6/8
+ */
+public class BitmapMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.bitmap_main_activity);
+    }
+
+    public void changeBitmap(View view) {
+
+
+    }
+}
