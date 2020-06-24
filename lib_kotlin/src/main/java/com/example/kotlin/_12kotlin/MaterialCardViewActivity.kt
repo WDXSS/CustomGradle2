2526d3cd9361d0b05c4f6b53b47275384fdc2726
zhouchao <810753835@qq.com>
2020-06-24 20:56:50 +0800

    Snackbar 配合 CoordinatorLayout 使用

---
+++ lib_kotlin/src/main/java/com/example/kotlin/_12kotlin/MaterialCardViewActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kotlin._12kotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlin.R
+
+/**
+ * @author zhouchao
+ * @date 2020/6/24
+ */
+class MaterialCardViewActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.kotlin_12_material_cardview_activity)
+    }
+}
