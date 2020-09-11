3bd306103ff39268d7ffa96d7795e4bfe9b84f01
ext.zhouchao3 <ext.zhouchao3@jd.com>
2020-09-11 15:54:31 +0800

    CoordinatorLayout  Group

--- customview/src/main/java/com/example/customview/view/constraint/ConstrainGroupActivity.java
+++ customview/src/main/java/com/example/customview/view/constraint/ConstrainGroupActivity.java
@@ -6,6 +6,7 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -26,12 +27,14 @@ public class ConstrainGroupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_constrain_layout_group);
 
 //        组的作用是可将多个控件一起隐藏或显示 。如下有三个 TextView
 
         mGroup = findViewById(R.id.group);
         mTextView1 = findViewById(R.id.textView1);
+       LinearLayout linearLayout =  findViewById(R.id.include);
 
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,13 +48,27 @@ public class ConstrainGroupActivity extends AppCompatActivity {
                 }
                 //代码中设置隐藏显示，一定记得 调用 requestLayout()函数
                 mGroup.requestLayout();
-//                if(mTextView1.getVisibility() == View.VISIBLE){
-//                    Log.i(TAG, "mTextView1:  if ");
-//                    mTextView1.setVisibility(View.INVISIBLE);
-//                }else{
-//                    mTextView1.setVisibility(View.VISIBLE);
-//                    Log.i(TAG, "mTextView1:  else ");
-//                }
+
+            }
+        });
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTextView1.getVisibility() == View.VISIBLE) {
+                    Log.i(TAG, "mTextView1:  if ");
+                    mTextView1.setVisibility(View.INVISIBLE);
+                } else {
+                    mTextView1.setVisibility(View.VISIBLE);
+                    Log.i(TAG, "mTextView1:  else ");
+                }
+
+                if (linearLayout.getVisibility() == View.VISIBLE) {
+                    Log.i(TAG, "linearLayout:  if ");
+                    linearLayout.setVisibility(View.INVISIBLE);
+                } else {
+                    linearLayout.setVisibility(View.VISIBLE);
+                    Log.i(TAG, "linearLayout:  else ");
+                }
             }
         });
 
