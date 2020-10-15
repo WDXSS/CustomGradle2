5b10fbffe16b0fbb5f0fb3f76cb909d9016a5054
ext.zhouchao3 <ext.zhouchao3@jd.com>
2020-10-15 18:59:42 +0800

    初探motionLayout

--- customview/src/main/java/com/example/customview/view/constraint/MotionLayoutActivity.java
+++ customview/src/main/java/com/example/customview/view/constraint/MotionLayoutActivity.java
@@ -7,6 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.customview.R;
 
+/**
+ * MotionLayoutActivity简介
+ * 官方 example
+ * https://github.com/android/views-widgets-samples/tree/master/ConstraintLayoutExamples
+ * 官方文档
+ * https://developer.android.com/training/constraint-layout/motionlayout
+ * 学习
+ * https://blog.csdn.net/knight1996/article/details/108015536
+ * @author ext.zhouchao3
+ * @date 2020-10-13 10:18
+ */
 public class MotionLayoutActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
