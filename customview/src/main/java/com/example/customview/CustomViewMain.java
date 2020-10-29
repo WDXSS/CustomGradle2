aa93945f2bba1eecbd12dcc4a344d21d96ee548d
ext.zhouchao3 <ext.zhouchao3@jd.com>
2020-10-29 18:44:45 +0800

    paging

--- customview/src/main/java/com/example/customview/CustomViewMain.java
+++ customview/src/main/java/com/example/customview/CustomViewMain.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -45,60 +46,64 @@ import pl.droidsonroids.gif.GifImageView;
 
 
 public class CustomViewMain extends AppCompatActivity {
-    private static final String TAG = "CustomViewMain";
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            "android.permission.READ_EXTERNAL_STORAGE",
-            "android.permission.WRITE_EXTERNAL_STORAGE"};
-    @MyTag(name = "BMW", size = 100)
-     Cat mCat;
+	private static final String TAG = "CustomViewMain";
+	private static final int REQUEST_EXTERNAL_STORAGE = 1;
+	private static String[] PERMISSIONS_STORAGE = {
+			"android.permission.READ_EXTERNAL_STORAGE",
+			"android.permission.WRITE_EXTERNAL_STORAGE"};
+	@MyTag(name = "BMW", size = 100)
+	Cat mCat;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //蜘蛛网
-        setContentView(R.layout.layout_spider);
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//蜘蛛网
+		setContentView(R.layout.layout_spider);
 //        setContentView(R.layout.contraint09);
-        notification();
-        getSupperClass();
-        verifyStoragePermissions(this);
+		notification();
+		getSupperClass();
+		verifyStoragePermissions(this);
 //        LogCollector.getInstance(getApplication()).start();
-        GifImageView gifImageView = findViewById(R.id.main_balance_img);
-        TextView textView = findViewById(R.id.text);
-        textView.setText(null);
+		GifImageView gifImageView = findViewById(R.id.main_balance_img);
+		TextView textView = findViewById(R.id.text);
+		textView.setText(null);
 
-        AnnotationCar.instance().inject(this);
-        Log.e("WANG", "Car is " + mCat.toString());
-        NumberUtil.decimalScale();
-    }
+		AnnotationCar.instance().inject(this);
+		Log.e("WANG", "Car is " + mCat.toString());
+		NumberUtil.decimalScale();
+
+		Log.e(TAG, "onCreate: text = " + textView.getContext().getClass().getSimpleName());
+		AppCompatButton appCompatButton = findViewById(R.id.appCompatBtn);
+		Log.e(TAG, "onCreate: appCompatButton = " +appCompatButton.getContext().getClass().getSimpleName());
+	}
 
 
-    private void luhnTest() {
-        EditText editText = findViewById(R.id.luhn);
-        String cardNo = editText.getText().toString();
-        boolean b = NumberUtil.LuhnCheck(cardNo);
-        Log.d(TAG, "onCreate: Luhn 算法 的结果 ：b = " + b);
-    }
+	private void luhnTest() {
+		EditText editText = findViewById(R.id.luhn);
+		String cardNo = editText.getText().toString();
+		boolean b = NumberUtil.LuhnCheck(cardNo);
+		Log.d(TAG, "onCreate: Luhn 算法 的结果 ：b = " + b);
+	}
 
-    private void notification() {
-        if (getIntent() != null && getIntent().getExtras() != null) {
-            String title = (String) getIntent().getExtras().get("title");
-            String body = (String) getIntent().getExtras().get("body");
-            System.out.println("title = " + title + ", body = " + body);
-            System.out.println("shuju = " + getIntent().getExtras());
-            System.out.println("actionClick = " + getIntent().getExtras().getString("click_action"));
-        }
+	private void notification() {
+		if (getIntent() != null && getIntent().getExtras() != null) {
+			String title = (String) getIntent().getExtras().get("title");
+			String body = (String) getIntent().getExtras().get("body");
+			System.out.println("title = " + title + ", body = " + body);
+			System.out.println("shuju = " + getIntent().getExtras());
+			System.out.println("actionClick = " + getIntent().getExtras().getString("click_action"));
+		}
 
-        if (getIntent().getExtras() != null) {
-            for (String key : getIntent().getExtras().keySet()) {
-                Log.i(TAG, "printlnMessage: key = " + key + ",value = " + getIntent().getExtras().get(key));
-            }
-        }
-    }
+		if (getIntent().getExtras() != null) {
+			for (String key : getIntent().getExtras().keySet()) {
+				Log.i(TAG, "printlnMessage: key = " + key + ",value = " + getIntent().getExtras().get(key));
+			}
+		}
+	}
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public void startBookMain(View view) {
-        startActivity(new Intent(CustomViewMain.this, BookMainActivity.class));
+	@RequiresApi(api = Build.VERSION_CODES.N)
+	public void startBookMain(View view) {
+		startActivity(new Intent(CustomViewMain.this, BookMainActivity.class));
 //        luhnTest();
 //        generateNum5();
 //        generateUnpredictableNumber();
@@ -108,185 +113,185 @@ public class CustomViewMain extends AppCompatActivity {
 //            Log.d(TAG, "startBookMain: random "+ (random.nextInt(10)));
 //        }
 
-    }
+	}
 
-    public void startUISystem(View view) {
-        startActivity(new Intent(CustomViewMain.this, MainActivity.class));
-    }
+	public void startUISystem(View view) {
+		startActivity(new Intent(CustomViewMain.this, MainActivity.class));
+	}
 
-    public void startDrawMain(View view) {
-        startActivity(new Intent(CustomViewMain.this, DrawViewMain.class));
-    }
+	public void startDrawMain(View view) {
+		startActivity(new Intent(CustomViewMain.this, DrawViewMain.class));
+	}
 
-    public void startHuangShu(View view) {
-        startActivity(new Intent(CustomViewMain.this, HuangShuMainActivity.class));
-    }
+	public void startHuangShu(View view) {
+		startActivity(new Intent(CustomViewMain.this, HuangShuMainActivity.class));
+	}
 
-    public void startNotify(View view) {
-        startActivity(new Intent(CustomViewMain.this, NotifyMain.class));
-    }
+	public void startNotify(View view) {
+		startActivity(new Intent(CustomViewMain.this, NotifyMain.class));
+	}
 
-    public void startFireBase(View view) {
-        startActivity(new Intent(CustomViewMain.this, FireMainActivity.class));
-    }
-
-
-    private void getSupperClass() {
+	public void startFireBase(View view) {
+		startActivity(new Intent(CustomViewMain.this, FireMainActivity.class));
+	}
 
 
-    }
+	private void getSupperClass() {
 
-    public void startFragment(View view) {
-        startActivity(new Intent(CustomViewMain.this, FragmentMainActivity.class));
-    }
 
-    public void startListView(View view) {
-        startActivity(new Intent(CustomViewMain.this, ListMainActivity.class));
-    }
+	}
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        Log.i(TAG, "onRequestPermissionsResult: requestCode = " + requestCode);
-        for (String permission : permissions) {
-            Log.i(TAG, "onRequestPermissionsResult: permission = " + permission);
-        }
-        for (int grantResult : grantResults) {
-            Log.i(TAG, "onRequestPermissionsResult: grantResult = " + grantResult);
-        }
-    }
+	public void startFragment(View view) {
+		startActivity(new Intent(CustomViewMain.this, FragmentMainActivity.class));
+	}
 
-    public void reqPermission(View view) {
-        DeviceInfo.reqPermission(CustomViewMain.this);
-    }
+	public void startListView(View view) {
+		startActivity(new Intent(CustomViewMain.this, ListMainActivity.class));
+	}
 
-    public void checkPermission(View view) {
-        DeviceInfo.getImeiNew(CustomViewMain.this);
-    }
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+		Log.i(TAG, "onRequestPermissionsResult: requestCode = " + requestCode);
+		for (String permission : permissions) {
+			Log.i(TAG, "onRequestPermissionsResult: permission = " + permission);
+		}
+		for (int grantResult : grantResults) {
+			Log.i(TAG, "onRequestPermissionsResult: grantResult = " + grantResult);
+		}
+	}
 
-    public void lifecycle(View view) {
+	public void reqPermission(View view) {
+		DeviceInfo.reqPermission(CustomViewMain.this);
+	}
+
+	public void checkPermission(View view) {
+		DeviceInfo.getImeiNew(CustomViewMain.this);
+	}
+
+	public void lifecycle(View view) {
 //        startActivity(new Intent(this, LifecycleActivity.class));
 //        startActivity(new Intent(this, LiveDataMainActivity.class));
-        startActivity(new Intent(this, JetpackMainActivity.class));
-    }
+		startActivity(new Intent(this, JetpackMainActivity.class));
+	}
 
-    public void startInclude(View view) {
-        startActivity(new Intent(CustomViewMain.this, IncludeMainActivity.class));
-    }
+	public void startInclude(View view) {
+		startActivity(new Intent(CustomViewMain.this, IncludeMainActivity.class));
+	}
 
-    public void startWeb(View view) {
-        startActivity(new Intent(CustomViewMain.this, WebViewActivity.class));
-    }
+	public void startWeb(View view) {
+		startActivity(new Intent(CustomViewMain.this, WebViewActivity.class));
+	}
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        Log.d(TAG, "onWindowFocusChanged() called with: hasFocus = [" + hasFocus + "]");
-    }
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		Log.d(TAG, "onWindowFocusChanged() called with: hasFocus = [" + hasFocus + "]");
+	}
 
 
-    public void coordinator(View view) {
-        startActivity(new Intent(CustomViewMain.this, CoordinatorActivity.class));
-    }
+	public void coordinator(View view) {
+		startActivity(new Intent(CustomViewMain.this, CoordinatorActivity.class));
+	}
 
-    public void immerisive(View view) {
-        //c沉浸式
-        startActivity(new Intent(CustomViewMain.this, ImmersionActivity.class));
-    }
+	public void immerisive(View view) {
+		//c沉浸式
+		startActivity(new Intent(CustomViewMain.this, ImmersionActivity.class));
+	}
 
-    public void startCoordinatorLayout(View view) {
-        startActivity(new Intent(CustomViewMain.this, CoordinatorLayoutMainActivity.class));
-    }
+	public void startCoordinatorLayout(View view) {
+		startActivity(new Intent(CustomViewMain.this, CoordinatorLayoutMainActivity.class));
+	}
 
-    private boolean isCall = false;
+	private boolean isCall = false;
 
-    public void callPhone(View view) {
+	public void callPhone(View view) {
 
-        if (checkReadPermission(Manifest.permission.CALL_PHONE, 10111)) {
+		if (checkReadPermission(Manifest.permission.CALL_PHONE, 10111)) {
 //            Intent intents [] = new Intent[2];
 //            Intent intent = new Intent(this,ImmersionActivity.class);
 //            intents[0] = intent;
-            //拨打电话
-            Intent intent1 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + 12345566));
-            startActivityForResult(intent1, 9999);
-            isCall = true;
+			//拨打电话
+			Intent intent1 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + 12345566));
+			startActivityForResult(intent1, 9999);
+			isCall = true;
 //
 //            intents[1] = intent1;
 //            startActivities(intents);
-        }
+		}
 
-    }
+	}
 
-    public boolean checkReadPermission(String string_permission, int request_code) {
-        boolean flag = false;
-        if (ContextCompat.checkSelfPermission(this, string_permission) == PackageManager.PERMISSION_GRANTED) {//已有权限
-            flag = true;
-        } else {//申请权限
-            ActivityCompat.requestPermissions(this, new String[]{string_permission}, request_code);
-        }
-        return flag;
-    }
-
-
-    public static void verifyStoragePermissions(Activity activity) {
-
-        try {
-            //检测是否有写的权限
-            int permission = ActivityCompat.checkSelfPermission(activity,
-                    "android.permission.WRITE_EXTERNAL_STORAGE");
-            if (permission != PackageManager.PERMISSION_GRANTED) {
-                // 没有写的权限，去申请写的权限，会弹出对话框
-                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        Log.d(TAG, "onNewIntent() called with: intent = [" + intent + "]");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart: ");
-
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart: ");
-        if (isCall) {
-            isCall = false;
-            Intent intent = new Intent(this, ImmersionActivity.class);
-            startActivity(intent);
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume: ");
-    }
+	public boolean checkReadPermission(String string_permission, int request_code) {
+		boolean flag = false;
+		if (ContextCompat.checkSelfPermission(this, string_permission) == PackageManager.PERMISSION_GRANTED) {//已有权限
+			flag = true;
+		} else {//申请权限
+			ActivityCompat.requestPermissions(this, new String[]{string_permission}, request_code);
+		}
+		return flag;
+	}
 
 
-    public void startRegexActivity(View view) {
-        Intent intent = new Intent(this, RegexMainActivity.class);
-        startActivity(intent);
-    }
+	public static void verifyStoragePermissions(Activity activity) {
 
-    public void startBitmapMainActivity(View view) {
-        Intent intent = new Intent(this, BitmapMainActivity.class);
-        startActivity(intent);
-    }
+		try {
+			//检测是否有写的权限
+			int permission = ActivityCompat.checkSelfPermission(activity,
+					"android.permission.WRITE_EXTERNAL_STORAGE");
+			if (permission != PackageManager.PERMISSION_GRANTED) {
+				// 没有写的权限，去申请写的权限，会弹出对话框
+				ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void startConstrainLayoutActivity(View view) {
-        Intent intent = new Intent(this, ConstraintLayoutActivity.class);
-        startActivity(intent);
-    }
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		Log.d(TAG, "onNewIntent() called with: intent = [" + intent + "]");
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart: ");
+
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Log.d(TAG, "onRestart: ");
+		if (isCall) {
+			isCall = false;
+			Intent intent = new Intent(this, ImmersionActivity.class);
+			startActivity(intent);
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume: ");
+	}
+
+
+	public void startRegexActivity(View view) {
+		Intent intent = new Intent(this, RegexMainActivity.class);
+		startActivity(intent);
+	}
+
+	public void startBitmapMainActivity(View view) {
+		Intent intent = new Intent(this, BitmapMainActivity.class);
+		startActivity(intent);
+	}
+
+	public void startConstrainLayoutActivity(View view) {
+		Intent intent = new Intent(this, ConstraintLayoutActivity.class);
+		startActivity(intent);
+	}
 
 
 }
