1878313bd8d28565464115e3f550416d341f8037
zc_mi <810753835@qq.com>
2020-11-01 17:40:59 +0800

    jetpack 上的

---
+++ jetpack/src/main/java/com/example/jetpack/lifecycle/MyApplication.kt
@@ -0,0 +1,14 @@
+package com.example.jetpack.lifecycle
+
+import android.app.Application
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.example.jetpack.lifecycle.imp.ApplicationObserver
+
+class MyApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+//        ApplicationObserver 继承自 LifecycleObserver
+        ProcessLifecycleOwner.get().lifecycle.addObserver(ApplicationObserver())
+    }
+}
