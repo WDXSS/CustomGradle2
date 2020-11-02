3ce4b9fb7637143d9d89e281626b003bdc5e6433
ext.zhouchao3 <ext.zhouchao3@jd.com>
2020-11-02 15:36:47 +0800

    java 8

--- lib_java/src/main/java/com/example/lib_java/java_8/IConvert.java
+++ lib_java/src/main/java/com/example/lib_java/java_8/IConvert.java
@@ -1,5 +1,9 @@
 package com.example.lib_java.java_8;
-@FunctionalInterface
+
+/**
+ * @FunctionalInterface 注解要求接口有且只有一个抽象方法，JDK中有许多类用到该注解，比如 Runnable，它只有一个 Run 方法。
+ */
+//@FunctionalInterface
 public interface IConvert<F,T> {
 	T convert(F form);
 }
